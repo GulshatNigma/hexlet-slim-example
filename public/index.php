b51a19c047f018c31bcec10c9f75f61670366fc3
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-01-11 15:58:49 +0500

    add /users

--- public/index.php
+++ public/index.php
@@ -11,4 +11,13 @@ $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
 });
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
 $app->run();
