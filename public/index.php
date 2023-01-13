942cbfc2a8bfe3c50352c1decdb9c9d038a228b8
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-01-13 16:58:53 +0500

    update index.php and add show.phptml

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . "/../vendor/autoload.php";
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -13,11 +20,17 @@ $app->get('/', function ($request, $response) {
 });
 
 $app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+    return $response->withStatus(302)->write('GET /users');
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
