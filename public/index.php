35d2c31992be0aa5ef769947f509c98c0901b45a
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-01-20 13:59:12 +0500

    add form

--- public/index.php
+++ public/index.php
@@ -19,15 +19,6 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
-/*$app->get('/users', function ($request, $response) {
-    return $response->withStatus(302)->write('GET /users');
-});*/
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
-
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
@@ -43,4 +34,48 @@ $app->get('/users', function ($request, $response) {
     return  $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
+$app->post('/users', function ($request, $response) {
+    $user = $request->getParsedBodyParam('user');
+    $id = uniqid();
+    $errors = validate($user);
+
+    $params = [
+        'id' => $id,
+        'nickname' => $user['nickname'],
+        'email' => $user['email'],
+        'errors' => $errors
+    ];
+
+    if (count($errors) === 0) {
+        $file = 'public/users.json';
+        $current = file_get_contents($file);
+        $current .= json_encode($params) . "\n";
+        file_put_contents($file, $current);
+
+        return $response->withRedirect('/users', 302);
+    }
+});
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['nickname' => '', 'email' => ''],
+        'errors' => []
+    ];
+    return  $this->get('renderer')->render($response, 'users/newUser.phtml', $params);
+});
 $app->run();
+
+function validate($user)
+{
+    $errors = [];
+
+    if (empty($user['nickname'])) {
+        $errors['nickname'] = "Can't be blank";
+    }
+
+    if (empty($user['email'])) {
+        $errors['email'] = "Can't be blank";
+    }
+
+    return $errors;
+}
